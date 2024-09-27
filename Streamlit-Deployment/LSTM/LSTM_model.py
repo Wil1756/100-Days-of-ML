de706ebdff37c18c6ad3468e1aa7dd8bb1b28581
John William <johnwilliams1756@gmail.com>
2024-09-27 21:47:28 +0300

    Build the User Interface

---
+++ Streamlit-Deployment/LSTM/LSTM_model.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+tfds.disable_progress_bar()
+
+# Helper function to display metric graphs
+def plot_graphs(history, metric):
+  plt.plot(history.history[metric])
+  plt.plot(history.history['val_'+metric], '')
+  plt.xlabel("Epochs")
+  plt.ylabel(metric)
+  plt.legend([metric, 'val_'+metric])
+  
+# Input pipeline 
+dataset, info = tfds.load('imdb_reviews', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+train_dataset.element_spec
+
+for example, label in train_dataset.take(1):
+  print('text: ', example.numpy())
+  print('label: ', label.numpy())
